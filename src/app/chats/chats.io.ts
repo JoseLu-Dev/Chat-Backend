b6a72b53cd1fc8eadd0b7e46db00aba0a1e460e2
JoseLu-Dev <joseludev@gmail.com>
2021-07-28 19:13:26 +0200

    Improved chat Differenced between received and sended messages

--- src/app/chats/chats.io.ts
+++ src/app/chats/chats.io.ts
@@ -1,9 +1,17 @@
 import { Server as IoServer, Socket } from "socket.io";
 import { SocketsEvents } from "../helpers/enums.helpers";
-import Message from "./message.model";
+import { Message } from "./message.model";
 
 export default function chatSockets(io: IoServer, socket: Socket) {
+    socket.emit(SocketsEvents.identification, getId());
     socket.on(SocketsEvents.message, (message: Message) => {
         io.emit(SocketsEvents.message, message)
     })
+}
+
+const usersID = [0]
+
+function getId() {
+    usersID.push(usersID[usersID.length-1] + 1);
+    return usersID[usersID.length-1];
 }
--- src/app/chats/message.model.ts
+++ src/app/chats/message.model.ts
@@ -1,9 +1,28 @@
-export default class Message {
-    sender: string;
+export class Message {
+    sender?: Sender ;
     content: string;
+    date: Date;
+    sended: boolean;
 
-    constructor(content: string, sender: string){
+    constructor(content: string, sender?: Sender, date?: Date) {
         this.sender = sender;
         this.content = content;
+
+        if (!date) {
+            this.date = new Date();
+        }else{
+            this.date = new Date(date);
+        }
+
+        this.sended = false;
+    }
+}
+
+export class Sender{
+    name?: string;
+    id?: number;
+
+    constructor(){
+
     }
 }
