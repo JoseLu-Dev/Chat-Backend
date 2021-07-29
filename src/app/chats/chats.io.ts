288a2d9ae1b4910bfa5e0a5663380127d3037dcd
JoseLu-Dev <joseludev@gmail.com>
2021-07-29 17:03:26 +0200

    Documented code

--- src/app/chats/chats.io.ts
+++ src/app/chats/chats.io.ts
@@ -2,16 +2,28 @@ import { Server as IoServer, Socket } from "socket.io";
 import { SocketsEvents } from "../helpers/enums.helpers";
 import { Message } from "./message.model";
 
+/**
+ * Sets all chat events to the socket connected and the server
+ * 
+ * @param io Socket.io server
+ * @param socket connected socket
+ */
 export default function chatSockets(io: IoServer, socket: Socket) {
-    socket.emit(SocketsEvents.identification, getId());
+    // sends a userID to identify each user on the frontend app
+    socket.emit(SocketsEvents.identification, getNewUserId());
+
+    // sends a message back to all sockets when one is received
     socket.on(SocketsEvents.message, (message: Message) => {
-        io.emit(SocketsEvents.message, message)
+        io.emit(SocketsEvents.message, message);
     })
 }
 
 const usersID = [0]
 
-function getId() {
+/**
+ * @returns a new user ID
+ */
+function getNewUserId() {
     usersID.push(usersID[usersID.length-1] + 1);
     return usersID[usersID.length-1];
 }
--- src/app/sockets.ts
+++ src/app/sockets.ts
@@ -2,6 +2,11 @@ import { Server as IoServer, Socket } from "socket.io";
 import chatSockets from "./chats/chats.io";
 import { SocketsEvents } from "./helpers/enums.helpers";
 
+/**
+ * Sets all events that will handle the socket server
+ * 
+ * @param io Socket.io server
+ */
 export default function initializeSockets(io: IoServer) {
     io.on(SocketsEvents.connection, (socket: Socket) => {
         chatSockets(io, socket)
